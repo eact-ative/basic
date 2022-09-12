c0aaf6285c9b31735ef37f372505a1b13a7fa4f1
GitNiko <galaxis.ling@gmail.com>
2022-09-12 20:10:14 +0800

    load ttf

--- android/rn/src/main/java/com/eactative/ua/rn/RNManager.kt
+++ android/rn/src/main/java/com/eactative/ua/rn/RNManager.kt
@@ -2,18 +2,24 @@ package com.eactative.ua.rn
 
 import android.app.Application
 import android.content.Context
+import android.graphics.Typeface
 import android.util.Log
+import com.eactative.resource.ResourceManager
 import com.eactative.ua.Constants
 import com.eactative.ua.entity.AppInfo
 import com.eactative.ua.entity.ModuleInfo
 import com.eactative.ua.entity.ResponseCode
 import com.eactative.ua.service.UAService
+import com.facebook.react.ReactInstanceEventListener
 import com.facebook.react.ReactInstanceManager
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.views.text.ReactFontManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 
 class RNManager {
@@ -42,6 +48,50 @@ class RNManager {
             return reactInstanceManager
         }
 
+        suspend fun pre(context: Context, application: Application, moduleInfo: ModuleInfo): ReactInstanceManager? {
+            val moduleID = moduleInfo.moduleID
+            if(reactInstanceManagerPool[moduleID] != null) {
+                return reactInstanceManagerPool[moduleID]
+            }
+            if(moduleInfo.script.isEmpty()) {
+                Log.e(Constants.TAG_UA_RN, "module info script is empty: $moduleInfo")
+                return null
+            }
+            val baseScript = moduleInfo.script[0]
+            if(baseScript.src == "") {
+                Log.e(Constants.TAG_UA_RN, "base script source is empty: $moduleInfo")
+                return null
+            }
+            val baseScriptSrc = ResourceManager.getResource(context, baseScript.src) ?: return null
+            var mainApplicationReactNativeHost = MainApplicationReactNativeHost(application)
+            mainApplicationReactNativeHost.jsBundleFile = baseScriptSrc
+            mainApplicationReactNativeHost.jsMainModuleName = "index"
+            val reactInstanceManager = mainApplicationReactNativeHost.reactInstanceManager
+
+            reactInstanceManager.addReactInstanceEventListener(object: ReactInstanceEventListener {
+                override fun onReactContextInitialized(context: ReactContext?) {
+                    if (context == null) {
+                        return
+                    }
+                    moduleInfo.script.forEachIndexed{i, script ->
+                        if(i == 0) {
+                            return
+                        }
+                        if(script.src == "") {
+                            Log.e(Constants.TAG_UA_RN, "module info script source is empty: $moduleInfo")
+                            return
+                        }
+                        context.catalystInstance.loadScriptFromFile(script.src, script.src, false)
+                    }
+                }
+            })
+            reactInstanceManagerPool[moduleID] = reactInstanceManager
+            GlobalScope.launch(Dispatchers.Main) {
+                reactInstanceManager?.createReactContextInBackground()
+            }
+            return reactInstanceManager
+        }
+
         fun getAppInfo(id: String): AppInfo? {
             val retrofit = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
@@ -61,6 +111,19 @@ class RNManager {
                 }
             }
             return null
+        }
+
+        suspend fun loadFont(context: Context, moduleInfo: ModuleInfo) {
+            if (moduleInfo.ttf.isEmpty()) {
+                return
+            }
+            if(moduleInfo.ttf[0] == null) {
+                return
+            }
+            val ttf = ResourceManager.getResource(context, moduleInfo.ttf[0].src) ?: return
+            val reactFontManager = ReactFontManager.getInstance()
+            val typeface = Typeface.createFromFile(File(ttf))
+            reactFontManager.setTypeface("iconfont", 0, typeface)
         }
     }
 }
